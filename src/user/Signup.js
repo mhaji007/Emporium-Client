190424a6b9954d70491d3e80c7f71d08776479f5
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-06 10:05:51 -0400

    Implement sign up functionality

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Layout from '../core/Layout';
+import {API} from '../config';
 
 
 const Signup = () => {
@@ -12,27 +13,54 @@ const Signup = () => {
 
   });
 
-  const handleChange = (name, e) =>{
+  const {name, email, password} = values;
+
+  const handleChange = (name, event) =>{
     console.log(name);
-    console.log(e);
-    setValues({...values, error:false, [name]: e.target.value})
+    console.log(event);
+    setValues({...values, error:false, [name]: event.target.value})
+  }
+
+  const signup = (user) =>{
+    // console.log(name, email, password);
+    fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(user)
+    }
+    )
+    .then(response => {
+      return response.json()
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  const clickSubmit = (event) =>{
+    event.preventDefault();
+    signup({name, email, password});
+
   }
 
   const SignUpForm = () => (
     <form >
       <div className="form-group">
         <label className="text-muted">Name</label>
-        <input onChange={(e)=>handleChange('name', e)} type="text" className="form-control"/>
+        <input onChange={(event)=>handleChange('name', event)} type="text" className="form-control"/>
       </div>
       <div className="form-group">
         <label className="text-muted">Email</label>
-        <input onChange={(e) =>handleChange('email', e)} type="email" className="form-control"/>
+        <input onChange={(event) =>handleChange('email', event)} type="email" className="form-control"/>
       </div>
       <div className="form-group">
         <label className="text-muted">Password</label>
-        <input onChange={(e) =>handleChange('password', e)} type="password" className="form-control"/>
+        <input onChange={(event) =>handleChange('password', event)} type="password" className="form-control"/>
       </div>
-      <button className="btn btn-primary ">
+      <button onClick={clickSubmit} className="btn btn-primary ">
         Submit
       </button>
     </form>
