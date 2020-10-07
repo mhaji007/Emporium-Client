56163e7570ed7dfdab3e044b41bccbdc68e83281
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-07 19:05:28 -0400

    Fix sign in/sign out display bug

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,52 +1,72 @@
-import React from 'react';
+import React from "react";
 // withRouter to access props history (e.g., /home, /about)
-import {Link, withRouter} from 'react-router-dom';
-import {signout} from '../auth';
+import { Link, withRouter } from "react-router-dom";
+import { signout, isAuthenticated } from "../auth";
 
-import './Menu.module.css';
+import "./Menu.module.css";
 
 const isActive = (history, path) => {
   // if the browser's history location matches
   // the path that is sent in mark link
   // as active
-  if(history.location.pathname === path) {
-    return {color: '#ff9900'};
+  if (history.location.pathname === path) {
+    return { color: "#ff9900" };
   } else {
-    return {color: '#ffffff'};
+    return { color: "#ffffff" };
   }
-}
+};
 // history props comes from react-router-dom
 // through withRouter
- const Menu = ({history}) => {
+const Menu = ({ history }) => {
   return (
     <div>
       <ul className="nav nav-tabs bg-dark">
         <li className="nav-item">
-          <Link className="nav-link" style={isActive(history, '/')} to="/">
+          <Link className="nav-link" style={isActive(history, "/")} to="/">
             Home
           </Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">
-            Signin
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">
-            Signup
-          </Link>
-        </li>
-        <li className="nav-item">
-          <span className="nav-link" style={{cursor: 'pointer', color: '#ffffff'}} onClick={() => signout(()=>{
-            history.push("/");
-          })}>
-            Signout
-          </span>
-        </li>
+        {!isAuthenticated() && (
+          <>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/signin")}
+                to="/signin"
+              >
+                Signin
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={isActive(history, "/signup")}
+                to="/signup"
+              >
+                Signup
+              </Link>
+            </li>
+          </>
+        )}
+
+        {isAuthenticated() && (
+          <li className="nav-item">
+            <span
+              className="nav-link"
+              style={{ cursor: "pointer", color: "#ffffff" }}
+              onClick={() =>
+                signout(() => {
+                  history.push("/");
+                })
+              }
+            >
+              Signout
+            </span>
+          </li>
+        )}
       </ul>
     </div>
-
-  )
-  }
+  );
+};
 
 export default withRouter(Menu);
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -5,8 +5,8 @@ import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
     const [values, setValues] = useState({
-        email: "Mehdi@gmail.com",
-        password: "mmmm9",
+        email: "",
+        password: "",
         error: "",
         loading: false,
         redirectToReferrer: false
