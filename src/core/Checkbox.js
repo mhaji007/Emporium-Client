4addcf765e59d1706f05ee17ffdd4d4fc05b3630
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-10-12 12:58:19 -0400

    Add fixed price range

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 
+// Contains checkbox logic
 const Checkbox = ({ categories, handleFilters }) => {
   const [checked, setCheked] = useState([]);
 
+  // Grab category id and put them
+  // in an array so we can send it
+  // to the backend to get all the
+  // products based on category
+  // accepts category id
   const handleToggle = (c) => () => {
     // Return the first index or -1
     const currentCategoryId = checked.indexOf(c);
+    // Grab a copy of the state
     const newCheckedCategoryId = [...checked];
     // If currently checked was not already in checked state > push
     if (currentCategoryId === -1) {
       newCheckedCategoryId.push(c);
-      // else pull
+      // else pull, indicating the uncheck
     } else {
       newCheckedCategoryId.splice(currentCategoryId, 1);
     }
@@ -18,14 +25,18 @@ const Checkbox = ({ categories, handleFilters }) => {
     // Use the new updated checked category
     setCheked(newCheckedCategoryId);
     // Send the updated checked category to
-    // the parent for prcoessing via
+    // the parent for processing via
     // handleFilters function
     handleFilters(newCheckedCategoryId);
   };
 
+  // Loop through categoires and
+  // display them in a list with
+  // a label and an input
   return categories.map((c, i) => (
     <li key={i} className="list-unstyled">
       <input
+        // pass in category id
         onChange={handleToggle(c._id)}
         // c._id is used for displaying checked items
         // display tickmark only if the id
--- src/core/Shop.js
+++ src/core/Shop.js
@@ -1,18 +1,25 @@
-import React, {useState, useEffect} from "react";
-import Layout from './Layout';
-import Card from './Card';
-import {getCategories} from "./apiCore";
-import Checkbox from './Checkbox';
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import Card from "./Card";
+import { getCategories } from "./apiCore";
+import Checkbox from "./Checkbox";
+import {prices} from "./fixedPrices";
 
 // Component that sends request to backend
 // and displays products based on filters
 const Shop = () => {
-
+  // Store category ids in the state
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] },
+  });
+  // State holding all categories
+  // to be shown on the left sidebar
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
 
+  // Fetch categories from backend
   const init = () => {
-    getCategories().then(data => {
+    getCategories().then((data) => {
       if (data.error) {
         setError(data.error);
       } else {
@@ -21,41 +28,48 @@ const Shop = () => {
     });
   };
 
-  // Filter is an object containing an array of
+  // filter is an object containing an array of
   // categoryIds and an array of price ranges
-  // FilterBy is either by category or by price
+  // filterBy is either by category or by price
   const handleFilters = (filters, filterBy) => {
-    console.log("SHOP", filters, filterBy);
-  }
+    // console.log("SHOP", filters, filterBy);
+    const newFilters = { ...myFilters };
+    // Update filters with cateogry or price
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
+  };
 
   useEffect(() => {
     init();
-  },[])
+  }, []);
 
   return (
-    <Layout title="Shop" description="Find your jam" className="container-fluid">
-
+    <Layout
+      title="Shop"
+      description="Find your jam"
+      className="container-fluid"
+    >
       <div className="row">
-
         <div className="col-4">
-         {/* Left sidebar - category display */}
-         <h4>Filter by categories</h4>
-         <ul>
-           {/* Pass categories and handleFilters to Checkbox */}
-          {<Checkbox categories={categories} handleFilters={(filters)=>handleFilters(filters, 'category')}/>}
+          {/* Left sidebar - category display */}
+          <h4>Filter by categories</h4>
+          <ul>
+            {/* Pass categories and handleFilters to Checkbox */}
+            {
+              <Checkbox
+                categories={categories}
+                handleFilters={(filters) => handleFilters(filters, "category")}
+              />
+            }
           </ul>
         </div>
         <div className="col-8">
-            {/* Right sidebar -  */}
-
+          {/* Right sidebar -  */}
+          {JSON.stringify(myFilters)}
         </div>
-
       </div>
-
-
-
-  </Layout>
-  )
-}
+    </Layout>
+  );
+};
 
 export default Shop;
